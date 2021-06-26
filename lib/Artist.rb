77b2a66080a97cfb53868bb873ef6b38cf69aedf
Hannah Malm <hannahemalm616@gmail.com>
2021-06-26 12:37:30 -0400

    commit tree f732f436b6fc54c8c7187bc4656271b674890b33

---
+++ lib/Artist.rb
@@ -0,0 +1,44 @@
+class Artist
+    attr_accessor :name, :songs
+    @@all = []
+    
+    def initialize(name)
+      @name=name 
+      @songs = []
+    end 
+    
+    def self.all 
+      @@all 
+    end 
+    
+    def self.destroy_all
+      self.all.clear
+    end 
+    
+    def save 
+      self.class.all << self 
+    end 
+    
+    def self.create(name)
+        artist = self.new(name)
+      artist.save 
+      artist 
+    end 
+    
+    def add_song(song)
+      @songs << song 
+      song.artist = self
+      if name.nil?
+        self.artist = Aritst.new(name)
+      else 
+        self.artist.name = name
+      end 
+    end 
+    
+    def genres(song=nil)
+      Artist.song.collect do |song| 
+      Song.genre
+    end 
+  
+  end 
+  end 
